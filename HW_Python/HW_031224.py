1b8dc65860619e21a805f81c2377c33f33363e4d
W <Wladimir334@github.com>
2024-12-01 14:16:40 +0300

    HW

--- HW_Python/HW_031224.py
+++ HW_Python/HW_031224.py
@@ -1,48 +1,81 @@
-numbers = [4,6,7]
-def sum_numbers(numbers):
-    summ = sum(numbers)
-    return summ
+# from PIL.ImageChops import difference
+#
+# from python_base import set_numbers
+#
+# numbers = [4,6,7]
+# def sum_numbers(numbers):
+#     summ = sum(numbers)
+#     return summ
+#
+# sum_result = sum_numbers(numbers)
+# print(sum_result)
+#
+#
+# def mult_numbers(numbers):
+#     mult = 1
+#     for i in numbers:
+#         mult = mult * i
+#     return mult
+#
+# mult_result = mult_numbers(numbers)
+# print(mult_result)
+#
+#
+# def  maximum(numbers):
+#     max_number = max(numbers)
+#     return max_number
+#
+# max_result = maximum(numbers)
+# print(max_result)
+#
+#
+# def  minimum(numbers):
+#     min_number = min(numbers)
+#     return min_number
+#
+# min_result = minimum(numbers)
+# print(min_result)
+#
+# # ---------------------------
+#
+# strike = input(str('Enter the strike: '))
+#
+# def strike_value(strike):
+#     words = strike.split()
+#     total_words = len(words)
+#     length_word = sum(1 for l in words if len(l) > 2)
+#
+#
+#     return f'Total words: {total_words}', f'Words more then 2 symbols: {length_word}'
+#
+# strike_value = strike_value(strike)
+# print(strike_value)
+#
+# low_symbols = strike.lower()
+# print (low_symbols)
+#
+# upper_symbols = strike.upper()
+# print (upper_symbols)
 
-sum_result = sum_numbers(numbers)
-print(sum_result)
 
+#------------------------------
 
-def mult_numbers(numbers):
-    mult = 1
-    for i in numbers:
-        mult = mult * i
-    return mult
+set_a = [1, 30, 30, 25, 24, 30, 1, 27, 25, 40]
+uniq_numbers = list(set(set_a))
+print(uniq_numbers)
 
-mult_result = mult_numbers(numbers)
-print(mult_result)
+def new_numbers(uniq_numbers):
+    new_list = []
+    new = set(uniq_numbers)
+    for x in new:
+        if x > 24:
+            new_list.append(x)
 
+    return new_list
 
-def  maximum(numbers):
-    max_number = max(numbers)
-    return max_number
+result = new_numbers(uniq_numbers)
+print(result)
 
-max_result = maximum(numbers)
-print(max_result)
+dict_num = {
 
-
-def  minimum(numbers):
-    min_number = min(numbers)
-    return min_number
-
-min_result = minimum(numbers)
-print(min_result)
-
-
-
-strike = input(str('Enter the strike: '))
-
-def strike_value(strike):
-    words = strike.split()
-    total_words = len(words)
-    length_word = sum(1 for l in words if len(l) > 2)
-
-
-    return f'Total words: {total_words}', f'Words more then 2 symbols: {length_word}'
-
-strike_value = strike_value(strike)
-print(strike_value)
+}

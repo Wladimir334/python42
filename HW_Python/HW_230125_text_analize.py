f8a1749b9e62a220308c621732b1ac70064a90a7
W <Wladimir334@github.com>
2025-01-26 19:42:59 +0300

    HW

--- HW_Python/HW_230125_text_analize.py
+++ HW_Python/HW_230125_text_analize.py
@@ -1,35 +1,55 @@
-
+import json
 
 class TextAnalyzer:
-    my_file = open(file='poem.txt', mode='r', encoding='utf-8')
-    poem = my_file.read()
-    def __init__(self,my_file):
-        self.my_file = my_file
+    total = {}
+    with open(file='poem.txt', mode='r', encoding='utf-8') as file:
+        poem = file.read()
+
+    def __init__(self, poem, total):
+        self.poem = poem
+        self.total = total
+
+    def poem_length(self, poem='my_file.read()'):
+        return len(poem)
+    result = len(poem)
+    length = 'Количество знаков всего: ' + str(result)
+    print(length)
+    total[length] = {}
+
+    def letters_only(self, poem='my_file.read()'):
+        return sum(1 for x in poem if x.isalpha())
+    result2 = sum(1 for x in poem if x.isalpha())
+    letters = 'Количество только букв: ' + str(result2)
+    print(letters)
+    total[letters] = {}
 
 
-    def read_file(self, my_file):
-        my_file = open(file='poem.txt', mode='r', encoding='utf-8')
-
-        print(read.my_file)
-
-
-    def lines(self):
-        pass
+    def lines(self, poem='my_file.read()'):
+        return len(poem.splitlines())
+    result3 = len(poem.splitlines())
+    line = 'Количество строк: ' + str(result3)
+    print(line)
+    total[line] = {}
 
 
-    def symbols(self):
-        pass
-        # return sum(1 for x in poem if x.isalpha())
+    def full_lines(self, poem='my_file.read()'):
+        return sum(1 for line in poem.splitlines() if line.strip())
+    result4 = sum(1 for line in poem.splitlines() if line.strip())
+    fulls = 'Количество непустых строк: ' + str(result4)
+    print(fulls)
+    total[fulls] = {}
 
 
-    def words(self):
-        pass
+    def words(self, poem='my_file.read()'):
+        return len(poem.split())
+    result5 = len(poem.split())
+    word = 'Количество слов в тексте: ' + str(result5)
+    print(word)
+    total[word] = {}
 
+    print(str(total))
 
-    def write_to_file(self):
-        pass
+    def write_total(self):
+        with open("result.txt", "w") as f:
+            json.dump(self.total, f, indent=2)
 
-
-read = TextAnalyzer(my_file = open(file='poem.txt', mode='r', encoding='utf-8'))
-
-read.read_file()

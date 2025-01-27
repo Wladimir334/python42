efc5092ee7721eaf9d8a607ff8d9f0c2150ee1f8
W <Wladimir334@github.com>
2025-01-27 21:08:32 +0300

    HW

--- HW_Python/HW_230125_text_analize.py
+++ HW_Python/HW_230125_text_analize.py
@@ -1,4 +1,6 @@
 import json
+from collections import Counter
+import string
 
 class TextAnalyzer:
     total = {}
@@ -43,13 +45,57 @@ class TextAnalyzer:
     def words(self, poem='my_file.read()'):
         return len(poem.split())
     result5 = len(poem.split())
-    word = 'Количество слов в тексте: ' + str(result5)
-    print(word)
-    total[word] = {}
+    wordd = 'Количество слов в тексте: ' + str(result5)
+    print(wordd)
+    total[wordd] = {}
 
-    print(str(total))
+
+    def words_per_line(self, poem='my_file.read()'):
+        return [len(line.split()) for line in poem.splitlines()]
+    result6 = [len(line.split()) for line in poem.splitlines()]
+    word_per_l = "Слов в каждой строке:" + str(result6)
+    print(word_per_l)
+    total[word_per_l] = {}
+
+
+    def characters_per_line(self, poem='my_file.read()'):
+        return [len(line) for line in poem.splitlines()]
+    result7 = [len(line) for line in poem.splitlines()]
+    characters = "Символов в каждой строке:" + str(result7)
+    print(characters)
+    total[characters] = {}
+
+
+    def repeated_words(self, poem='my_file.read()'):
+        return Counter(poem.lower().translate(str.maketrans('', '', string.punctuation)).split())
+    result8 = Counter(poem.lower().translate(str.maketrans('', '', string.punctuation)).split())
+    repeated = "Повторяющиеся слова:" + str(result8)
+    print(repeated)
+    total[repeated] = {}
+
+    def letter_frequency_analysis(self, poem='my_file.read()'):
+        return Counter([char.lower() for char in poem if char.isalpha()])
+    result9 = Counter([char.lower() for char in poem if char.isalpha()])
+    letter_frequency = "Частотный анализ букв:" + str(result9)
+    print(letter_frequency)
+    total[letter_frequency] = {}
+
+
+    def find_non_letter_symbols(self, poem='my_file.read()'):
+        return Counter([char for char in poem if not char.isalnum()])
+    result10 = Counter([char for char in poem if not char.isalnum()])
+    find_non_letter = "Посторонние символы (пробелы и знаки препинания):" + str(result10)
+    print(find_non_letter)
+    total[find_non_letter] = {}
+
+
+    total2 = str(total)
+    print(total2)
+
 
     def write_total(self):
         with open("result.txt", "w") as f:
-            json.dump(self.total, f, indent=2)
+            f.write(self.total2)
+
+
 
